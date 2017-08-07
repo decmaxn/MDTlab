c58e9f44193052aa5180f14a68435b6d3d10844b
decmaxn <decmaxn@hotmail.com>
2017-08-07 13:45:39 -0400

    DSC script to do minimal configuration after install from DVD

---
+++ configureServer.ps1
@@ -0,0 +1,94 @@
+﻿<# Notes:
+
+Authors: Greg Shields
+Modified by Victor Ma
+
+Goal - Configure minimal initial settings for a server.
+This script must be run after prepServer.ps1
+
+Note:
+Please modify the variables in $ConfigData section, copy it down to local folder, and run it from there. 
+
+Error:
+ConvertTo-MOFInstance : System.InvalidOperationException error processing property 'Password' OF TYPE 'User': Converting  and storing encrypted passwords as plain text is not recommended
+Solution: https://blogs.technet.microsoft.com/ashleymcglone/2015/12/18/using-credentials-with-psdscallowplaintextpassword-and-psdscallowdomainuser-in-powershell-dsc-configuration-data/
+
+
+Disclaimer
+This example code is provided without copyright and AS IS.  It is free for you to use and modify.
+Note: These demos should not be run as a script. These are the commands that I use in the 
+demonstrations and would need to be modified for your environment.
+
+#>
+
+configuration configureServer
+{
+    Import-DscResource -ModuleName xComputerManagement, xNetworking
+    Node localhost
+    {
+
+        LocalConfigurationManager {
+            ActionAfterReboot = 'ContinueConfiguration'
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+  
+        xIPAddress NewIPAddress {
+            IPAddress = $node.IPAddress
+            InterfaceAlias = "ethernet"
+            #PrefixLength = 24
+            AddressFamily = "IPV4"
+        }
+
+        xDefaultGatewayAddress NewIPGateway {
+            Address = $node.GatewayAddress
+            InterfaceAlias = "ethernet"
+            AddressFamily = "IPV4"
+            DependsOn = '[xIPAddress]NewIPAddress'
+        }
+
+        xDnsServerAddress PrimaryDNSClient {
+            Address        = $node.DNSIPAddress
+            InterfaceAlias = "ethernet"
+            AddressFamily = "IPV4"
+            DependsOn = '[xDefaultGatewayAddress]NewIPGateway'
+        }
+
+        User Administrator {
+            Ensure = "Present"
+            UserName = "Administrator"
+            Password = $Cred
+            DependsOn = '[xDnsServerAddress]PrimaryDNSClient'
+        }
+
+        xComputer ChangeNameAndJoinDomain {
+            Name = $node.ThisComputerName
+            DomainName    = $node.DomainName
+            Credential    = $domainCred
+            DependsOn = '[User]Administrator'
+        }
+    }
+}
+            
+$ConfigData = @{
+    AllNodes = @(
+        @{
+            Nodename = "localhost"
+            ThisComputerName = "S16x64-MDT"
+            IPAddress = "192.168.0.56"
+            GatewayAddress = "192.168.0.1"
+            DNSIPAddress = "192.168.0.15"
+            DomainName = "HLAB.local"
+            PSDscAllowPlainTextPassword = $true
+            PSDscAllowDomainUser = $true
+        }
+    )
+}
+
+$domainCred = Get-Credential -Message "Please enter a new password for Domain Administrator."
+$Cred = Get-Credential -UserName Administrator -Message "Please enter a new password for Local Administrator and other accounts."
+
+configureServer -ConfigurationData $ConfigData
+
+Set-DSCLocalConfigurationManager -Path .\configureServer –Verbose
+Start-DscConfiguration -Wait -Force -Path .\configureServer -Verbose
---
+++ prepServer.ps1
@@ -0,0 +1,29 @@
+﻿<# Notes:
+
+Authors: Greg Shields
+Modified by Victor Ma
+
+Goal - Prepare the local machine by installing needed PowerShell Gallery modules.
+This script must be run before configureServer.
+
+Note:
+Get-PackageSource will error out in Windows Server 2016 version 1607 Build 14393.447 installation with GUI, but it's not happening on the installation without GUI.
+Error reported at https://github.com/OneGet/oneget/issues/183. 
+
+
+Disclaimer
+
+This example code is provided without copyright and AS IS.  It is free for you to use and modify.
+Note: These demos should not be run as a script. These are the commands that I use in the 
+demonstrations and would need to be modified for your environment.
+
+#>
+
+Get-PackageSource -Name PSGallery | Set-PackageSource -Trusted -Force -ForceBootstrap
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+Install-Module xComputerManagement -Force
+Install-Module xNetworking -Force
+
+Write-Host "You may now execute '.\configureServer.ps1'"
